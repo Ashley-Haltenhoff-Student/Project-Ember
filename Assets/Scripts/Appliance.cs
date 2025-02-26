d41facbf60775bf3e75d54f0f0ab7d26c5a558e7
AJ <144151461+Ashley-Haltenhoff-Student@users.noreply.github.com>
2025-02-26 14:46:14 -0500

    started trying to open the windows

--- Assets/Scripts/Appliance.cs
+++ Assets/Scripts/Appliance.cs
@@ -3,10 +3,22 @@ using UnityEngine.AI;
 
 public class Appliance : MonoBehaviour
 {
+    [SerializeField] private GameObject window; // gameplay window
+
     [SerializeField] private Player player;
+    public NavMeshAgent playerAgent;
+
+    private void Start()
+    {
+        player.GetComponent<NavMeshAgent>();
+    }
 
     private void OnMouseDown()
     {
         player.GetComponent<NavMeshAgent>().SetDestination(transform.position);
+        
+        if (window) { player.OpenWindow(gameObject.GetComponent<Appliance>()); }
     }
+
+    public GameObject GetApplianceWindow() { return window; }
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,8 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Player : MonoBehaviour
 {
+    private NavMeshAgent agent;
 
+    private void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    public void OpenWindow(Appliance appliance)
+    {
+        StartCoroutine(WaitToOpenWindow(appliance));
+    }
+
+    private IEnumerator WaitToOpenWindow(Appliance appliance)
+    {
+        while (new Vector3(agent.destination.x, agent.destination.y,0) == appliance.gameObject.transform.position)
+        {
+            // doesn't work
+            if (Vector2.Distance(transform.position, new Vector3(agent.destination.x, agent.destination.y, 0)) <= 0.5f + agent.stoppingDistance)
+            {
+                appliance.GetApplianceWindow().SetActive(true);
+            }
+            yield return null;
+        }
+
+        appliance.GetApplianceWindow().SetActive(false);
+    }
 }
